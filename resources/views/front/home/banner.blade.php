491788b309a4d2008f1a8b0517122288c9332a64
Michael Joyner <joyner.michael@gmail.com>
2018-12-18 11:09:07 +0800

    change banner logo height for mobile

--- resources/views/front/home/banner.blade.php
+++ resources/views/front/home/banner.blade.php
@@ -1,4 +1,4 @@
 <section class="banner-height home-banner flex justify-center items-center relative">
-    @include('svgs.logos.cuisine', ['svgClass' => 'h-48'])
+    @include('svgs.logos.cuisine', ['svgClass' => 'h-32 md:h-48'])
     @include('front.partials.banner-scroll-down')
 </section>
