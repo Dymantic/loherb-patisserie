b23542956615beab9769eada1c3140bcf0ac135d
Michael Joyner <joyner.michael@gmail.com>
2021-04-12 11:14:24 +0800

    wip

--- resources/views/front/home/instagram.blade.php
+++ resources/views/front/home/instagram.blade.php
@@ -2,7 +2,7 @@
     @include('front.partials.heading-charcoal', ['text' => trans('homepage.instagram.heading')])
     <div class="mt-12">
         @foreach($instagrams->take(8)->chunk(4) as $row)
-            <div class="max-w-xl mx-auto flex-wrap md:flex-no-wrap justify-center w-full flex">
+            <div class="max-w-4xl mx-auto flex-wrap md:flex-no-wrap justify-center w-full flex">
             @foreach($row as $instagram)
                 <div class="instagram-image-box">
                     <img data-src="{{ $instagram['url'] }}" class="lazyload"
